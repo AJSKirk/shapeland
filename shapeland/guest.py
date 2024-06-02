065435474dfc54cbe87d3c9ba447201cabb7f044
Andrew Kirk <ajskirk@gmail.com>
2024-06-02 22:40:31 +0200

    Unweighted homogenous choicemaking

--- shapeland/guest.py
+++ shapeland/guest.py
@@ -1,5 +1,5 @@
 import simpy as S
-from ride import Ride
+from ride import Activity, Ride
 
 import random
 from typing import Iterable
@@ -11,19 +11,45 @@ class Guest:
         self._id = id
 
         self.max_wait = max_wait
+        self.ride_preference = 0.5
+        self.ride_history = []
+        self.allow_repeats = True
 
-    def run(self, available_rides=Iterable[Ride]):
-        while True:
-            yield self.env.process(self.ride(random.choice(available_rides)))
+        self.entrance_time = self.env.now
+        self.time_preference = 4 * 60
 
-    def ride(self, ride: Ride):
-        #print(f"Guest {self._id} evaluating ride {ride.name}  at time {self.env.now} - wait is {ride.wait_time}")
+    def run(self, activities=Iterable[Activity], rides=Iterable[Ride]):
+        while not self.decide_to_leave():
+            # Decide on activity or ride
+            if random.uniform(0, 1) <= self.ride_preference:  # Ride
+                ride = random.choice(self.valid_rides(rides))
+                yield self.env.process(self.try_ride(ride))
+
+            else:  # Do an activity
+                # TODO: Make this a weighted choice
+                activity = random.choice(activities)
+                yield self.env.process(activity.do_activity())
+
+    def try_ride(self, ride: Ride):
         if ride.wait_time >= self.max_wait:
-            print(f"Guest {self._id} balked at wait time of {ride.wait_time} for {ride.name}")
             return
 
         with ride.request() as req:
             yield req  # Wait in queue
-            #print(f"Guest {self._id} started the ride at time {self.env.now}")
             yield self.env.process(ride.ride())
-            #print(f"Guest {self._id} finished the ride at time {self.env.now}")
+            self.ride_history.append(ride)
+
+    def valid_rides(self, rides: Iterable[Ride]):
+        allowed_rides = rides
+        if not self.allow_repeats:
+            # TODO: Also remove any FastPasses held
+            allowed_rides = [r for r in allowed_rides if r not in self.ride_history]
+
+        # TODO: Adult/child eligibility
+        return allowed_rides
+
+    def decide_to_leave(self):
+        if self.env.now == self.entrance_time:  # Never leave immediately
+            return False
+
+        return self.env.now - self.entrance_time - self.time_preference > random.gauss(0, 1)
